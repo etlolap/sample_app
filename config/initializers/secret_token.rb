680314322d14dbecf004e64a2f3a3efe60e3b9a5
Etl Olap <etl_olap@yahoo.com>
2011-10-30 17:02:04 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '61953fef1f7727c2d41d689fc0173fe07e30b531c853b209a6e12489fb74ff2306849a2bb387ed6023103eb9dffc1532fe92dae94ffb2fcc8a1b9e09c8a6f215'
