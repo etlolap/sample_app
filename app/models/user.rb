c00516ed0b3283564d6bf50c5ef264f318161000
Etl Olap <etl_olap@yahoo.com>
2011-11-27 13:51:52 -0800

    chap11 user microposts done

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,8 @@ class User < ActiveRecord::Base
 	attr_accessor :password
 	attr_accessible :name, :email, :password, :password_confirmation
 	
+	has_many :microposts, :dependent => :destroy
+	
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	
 	validates :name, :presence => true,
@@ -48,6 +50,11 @@ class User < ActiveRecord::Base
 		(user && user.salt == cookie_salt) ? user : nil
 	end
 	
+  def feed
+    # This is preliminary. See Chapter 12 for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
+  
 	private 
 		def encrypt_password
 			return if password.nil?
